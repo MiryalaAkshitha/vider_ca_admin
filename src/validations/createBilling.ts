c9328cafe9aac78798fbb9fc056d36c9700eff40
Shahid <shahidazam@janaspandana.in>
2022-06-07 19:12:42 +0530

    billing Entities form validations

--- src/validations/createBilling.ts
+++ src/validations/createBilling.ts
@@ -19,10 +19,14 @@ let CreateBillingSchema = object().shape({
         .max(12, "Display name should not exceed 12 characters"),
     category: string().required("Select a Billing Entity Category"),
     type: string().required("Select a Billing Entity Type"),
-    registrationNumber: string().notRequired(),
-    contactName: string().notRequired(),
-    email: string().notRequired(),
-    mobileNumber: string().notRequired(),
+    registrationNumber: string().required(),
+    contactName: string().required()
+        .min(3, "Display name should be atleast 3 characters")
+        .max(12, "Display name should not exceed 12 characters"),
+    email: string().email("Invalid email address").required("Email is required"),
+    mobileNumber: string().required("Mobile number is required")
+        .min(10, "Mobile number should be atleast 10 digits")
+        .max(10, "Mobile number should not exceed 10 digits"),
     gstRegistered: string().notRequired(),
     gstIn: string().notRequired(),
 
--- src/validations/createEntityDocument.ts
+++ src/validations/createEntityDocument.ts
@@ -4,18 +4,16 @@ let createEntityDocumentDefaultValues = {
     licenseType: "",
     licenseTitle: "",
     licenseNumber: "",
-    
-   
+
+
 
 };
 
 let CreateEntityDocumentSchema = object().shape({
-    bankName: string().notRequired(),
-    branchName: string().notRequired(),
-    accountNumber: string().notRequired(),
-    ifscCode: string().notRequired(),
-    upiId: string().notRequired(),
-    
+    licenseType: string().required(),
+    licenseTitle: string().required(),
+    licenseNumber: string().required(),
+
 
 });
 
