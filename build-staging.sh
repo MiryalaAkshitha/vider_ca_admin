78c782bd28bb2cf638e5fff0ce6061c5aad642af
vinay <vinay@janaspandana.in>
2022-06-21 16:56:07 +0530

    build-stagin.sh change

--- build-staging.sh
+++ build-staging.sh
@@ -1,5 +1,5 @@
 rm -rf build
 yarn build:staging
 cp ./apache.conf ./build/.htaccess
-ssh viderTest 'rm -rf /home/ubuntu/vider_ca_admin_staging/build'
-scp -r ./build viderTest:/home/ubuntu/vider_ca_admin_staging/build
+ssh viderTest 'rm -rf /home/ubuntu/vider_ca_admin/build'
+scp -r ./build viderTest:/home/ubuntu/vider_ca_admin/build
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,22 +1,21 @@
-import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { Box } from "@mui/material";
 import { getEvents } from "api/services/events";
 import { getTasks } from "api/services/tasks";
 import FloatingButton from "components/FloatingButton";
 import Loader from "components/Loader";
+import ValidateAccess from "components/ValidateAccess";
 import useTitle from "hooks/useTitle";
 import moment from "moment";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import { ResType } from "types";
+import { Permissions } from "utils/permissons";
 import AddEvent from "views/calendar/AddEvent";
 import ViewEvent from "views/calendar/ViewEvent";
-import ValidateAccess from "components/ValidateAccess";
-import { Permissions } from "utils/permissons";
-import { usePermissions } from "context/PermissionsProvider";
 
 function Calendar() {
   useTitle("Calendar");
