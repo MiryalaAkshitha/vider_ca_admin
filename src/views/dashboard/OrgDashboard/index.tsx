0effa664a2054ffa55d1e6c95ee37191bbf1842d
vinay <vinay@janaspandana.in>
2022-09-05 12:33:32 +0530

    hide dashboard

--- src/views/dashboard/OrgDashboard/index.tsx
+++ src/views/dashboard/OrgDashboard/index.tsx
@@ -1,31 +1,19 @@
 import { Box } from "@mui/material";
-import {
-  getOverdueTasks,
-  getTaskAnalytics,
-  getTasksByCategory,
-  getTasksByService,
-  getTasksDueThisWeek,
-} from "api/services/organization";
 import ComingSoon from "components/ComingSoon";
-import Loader from "components/Loader";
-import { useQuery } from "react-query";
-import ClientAnalytics from "./client-analytics";
-import LoghourAnalytics from "./loghour-analytics";
-import TaskAnalytics from "./task-analytics";
 
 function Dashboard() {
-  const { data, isLoading } = useQuery(["task-analytics"], getTaskAnalytics);
+  // const { data, isLoading } = useQuery(["task-analytics"], getTaskAnalytics);
 
-  const { data: some, isLoading: someLoading } = useQuery(
-    ["task-due-this-week"],
-    getTasksDueThisWeek
-  );
+  // const { data: some, isLoading: someLoading } = useQuery(
+  //   ["task-due-this-week"],
+  //   getTasksDueThisWeek
+  // );
 
-  const { data: ts, isLoading: tsLoading } = useQuery(["task-by-service"], getTasksByCategory);
+  // const { data: ts, isLoading: tsLoading } = useQuery(["task-by-service"], getTasksByCategory);
 
-  const { data: tc, isLoading: tcLoading } = useQuery(["task-by-service"], getTasksByService);
+  // const { data: tc, isLoading: tcLoading } = useQuery(["task-by-service"], getTasksByService);
 
-  const { data: ot, isLoading: otLoading } = useQuery(["over-due-tasks"], getOverdueTasks);
+  // const { data: ot, isLoading: otLoading } = useQuery(["over-due-tasks"], getOverdueTasks);
 
   // let grouped = _.groupBy(tasks, 'dueDate');
 
@@ -33,7 +21,7 @@ function Dashboard() {
   //   return moment(b).diff(moment(a));
   // });
 
-  if (isLoading) return <Loader />;
+  // if (isLoading) return <Loader />;
 
   return (
     <Box p={2}>
