4d64b1585b5e4235e5f743a9bf340fb8d16fa9e7
vivek <vivekmyadaram@vider.in>
2022-11-18 21:34:00 +0530

    email templates preview

--- src/pages/emails2.tsx
+++ src/pages/emails2.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Grid, MenuItem, TextField } from "@mui/material";
 import { http } from "api/http";
 import axios from "axios";
+import Loader from "components/Loader";
 import { useState } from "react";
+import { useQuery } from "react-query";
+import { ResType } from "types";
 
 function Emails() {
   const [state, setState] = useState({
@@ -11,6 +14,10 @@ function Emails() {
     body: "",
     templateid: "",
     atomID: "",
+  });
+
+  const { data, isLoading }: ResType = useQuery(["templates"], () => {
+    return http.get("/common/email-templates");
   });
 
   const handleChange = (e) => {
@@ -34,68 +41,94 @@ function Emails() {
       });
   };
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <Box p={3} maxWidth={600}>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          required
-          label="From Email"
-          onChange={handleChange}
-          variant="outlined"
-          fullWidth
-          name="fromAddress"
-        />
-        <TextField
-          required
-          label="To Email"
-          sx={{ mt: 2 }}
-          variant="outlined"
-          name="toAddress"
-          fullWidth
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Subject"
-          sx={{ mt: 2 }}
-          variant="outlined"
-          name="subject"
-          fullWidth
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Body"
-          sx={{ mt: 2 }}
-          variant="outlined"
-          name="body"
-          fullWidth
-          multiline
-          rows={5}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Template Id"
-          name="templateid"
-          sx={{ mt: 2 }}
-          variant="outlined"
-          fullWidth
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Atom Id"
-          name="atomID"
-          sx={{ mt: 2 }}
-          variant="outlined"
-          fullWidth
-          onChange={handleChange}
-        />
-        <Button type="submit" variant="contained" size="large" sx={{ mt: 2 }}>
-          Send
-        </Button>
-      </form>
+    <Box p={3}>
+      <Grid container>
+        <Grid item lg={6}>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              required
+              label="From Email"
+              onChange={handleChange}
+              variant="outlined"
+              fullWidth
+              name="fromAddress"
+            />
+            <TextField
+              required
+              label="To Email"
+              sx={{ mt: 2 }}
+              variant="outlined"
+              name="toAddress"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              required
+              label="Subject"
+              sx={{ mt: 2 }}
+              variant="outlined"
+              name="subject"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              required
+              label="Body"
+              sx={{ mt: 2 }}
+              variant="outlined"
+              name="body"
+              fullWidth
+              multiline
+              rows={5}
+              onChange={handleChange}
+            />
+            <TextField
+              required
+              select
+              label="Template Id"
+              name="templateid"
+              sx={{ mt: 2 }}
+              variant="outlined"
+              fullWidth
+              onChange={handleChange}
+            >
+              {data.data?.list.map((item) => {
+                return (
+                  <MenuItem key={item.id} value={item.name}>
+                    {item.name}
+                  </MenuItem>
+                );
+              })}
+            </TextField>
+            <TextField
+              required
+              label="Atom Id"
+              name="atomID"
+              sx={{ mt: 2 }}
+              variant="outlined"
+              fullWidth
+              onChange={handleChange}
+            />
+            <Button type="submit" variant="contained" size="large" sx={{ mt: 2 }}>
+              Send
+            </Button>
+          </form>
+        </Grid>
+        <Grid item lg={6}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: state?.templateid
+                ? data.data.list.find((item) => item.name == state.templateid).body
+                : "",
+            }}
+          ></div>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
