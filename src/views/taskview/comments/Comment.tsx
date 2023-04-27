02ce02f7e95881f78c02226eaf4c1209450b776c
vider <tech@vider.in>
2023-04-27 22:06:44 +0530

    prod changes fix

--- src/views/taskview/comments/Comment.tsx
+++ src/views/taskview/comments/Comment.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, IconButton, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Avatar, Button, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import moment from "moment";
 import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
@@ -10,6 +10,7 @@ import { useParams } from "react-router-dom";
 import { snack } from "components/toast";
 import { addComment } from "api/services/tasks/tasks";
 import { useMutation, useQueryClient } from "react-query";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const USER_REGEX = /@\[+[a-z\s]+\]/gi;
 
@@ -55,7 +56,136 @@ function TaskComment({ data, users }: Props) {
 
   return (
     <Box mt={4} borderBottom="1px solid rgba(0,0,0,0.08)" pb={3}>
-      <Box maxWidth={1000}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Box mb="4px" display="flex" gap={2} alignItems="center">
+            <Avatar src={data?.user?.imageUrl} />
+            <div>
+              <Typography variant="body1" sx={{ lineHeight: "18px" }}>
+                {data?.user?.fullName}
+              </Typography>
+              <Typography variant="caption" color="rgba(0,0,0,0.5)">
+                {moment(data?.createdAt).calendar()}
+              </Typography>
+            </div>
+          </Box>
+
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="body1" color="rgba(0,0,0,0.8)">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: getText(data?.text),
+              }}
+            ></span>
+          </Typography>
+          <Box display="flex" gap={3} mt={1}>
+            <Button
+              sx={{
+                minWidth: 80,
+                color: "rgba(0,0,0,0.7)",
+                background: showReply ? "rgba(0,0,0,0.08)" : "",
+              }}
+              size="small"
+              onClick={() => setShowReply(!showReply)}
+              startIcon={<ReplyRoundedIcon />}
+            >
+              Reply
+            </Button>
+            <Button
+              sx={{
+                minWidth: 80,
+                color: "rgba(0,0,0,0.7)",
+                background: showComments ? "rgba(0,0,0,0.08)" : "",
+              }}
+              size="small"
+              onClick={() => {
+                setShowComments(!showComments);
+              }}
+              startIcon={<CommentRoundedIcon fontSize="small" />}
+            >
+              Comments {data?.replies?.length > 0 && `(${data?.replies?.length})`}
+            </Button>
+          </Box>
+          {showReply && (
+            <Box position="relative" mt={1}>
+              <MentionsInput
+                autoFocus
+                className="mentions_input"
+                value={reply}
+                placeholder="Write a comment here and type @ to mention someone...."
+                onChange={(e: any) => {
+                  setReply(e.target.value);
+                }}
+              >
+                <Mention
+                  appendSpaceOnAdd
+                  markup="@[__display__]"
+                  trigger="@"
+                  displayTransform={(id: string, display: string) =>
+                    `@${display}`
+                  }
+                  style={{
+                    backgroundColor: "rgba(233, 107, 116, 0.2)",
+                    borderRadius: 4,
+                  }}
+                  data={users?.map((user: any) => ({
+                    id: user.id,
+                    display: user.fullName,
+                  }))}
+                />
+              </MentionsInput>
+              <Box position="absolute" right={20} bottom={5}>
+                <IconButton onClick={onReply} disabled={!reply}>
+                  <SendOutlined
+                    sx={{ opacity: reply ? 1 : "0.5" }}
+                    color="secondary"
+                  />
+                </IconButton>
+              </Box>
+            </Box>
+          )}
+          {showComments && data?.replies?.length > 0 && (
+            <Box mt={1} ml={2}>
+              {data?.replies?.map((item: any) => (
+                <Box mb={2}>
+                  <Box mb={1} display="flex" gap={2} alignItems="center">
+                    <Avatar
+                      sx={{ width: 30, height: 30 }}
+                      src={data?.user?.imageUrl}
+                    />
+                    <div>
+                      <Typography variant="body2" sx={{ lineHeight: "10px" }}>
+                        {item?.user?.fullName}
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        color="rgba(0,0,0,0.5)"
+                        sx={{ fontSize: 10 }}
+                      >
+                        {moment(item?.createdAt).calendar()}
+                      </Typography>
+                    </div>
+                  </Box>
+                  <Typography variant="body2" color="rgba(0,0,0,0.8)">
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: getText(item?.text),
+                      }}
+                    ></span>
+                  </Typography>
+                </Box>
+              ))}
+            </Box>
+          )}
+        </AccordionDetails>
+      </Accordion>
+
+      {/* <Box maxWidth={1000}>
         <Box mb="8px" display="flex" gap={2} alignItems="center">
           <Avatar src={data?.user?.imageUrl} />
           <div>
@@ -173,7 +303,7 @@ function TaskComment({ data, users }: Props) {
             ))}
           </Box>
         )}
-      </Box>
+      </Box> */}
     </Box>
   );
 }
