bb8da5386066b084b9445bbfced50cf61983de49
vider <tech@vider.in>
2023-06-02 22:46:25 +0530

    invoice create client profile billing addr mandatory hot fix

--- src/views/billing/estimates/AddEstimate/ClientDetails.tsx
+++ src/views/billing/estimates/AddEstimate/ClientDetails.tsx
@@ -39,7 +39,7 @@ function ClientDetails() {
       (item: any) => item.id === e.target.value
     );
     setCurclient(client);
-    if (client?.address && client?.address == null && (client?.address?.billingaddress?.locality == '')
+    if (client?.address && client?.address !== null && (client?.address?.billingaddress?.locality == '')
       && (client?.address?.billingaddress?.street == '')
       && (client?.address?.billingaddress?.city == '')
       && (client?.address?.billingaddress?.district == '')
